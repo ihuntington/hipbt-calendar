b34c02e5960e885ddcf4c6a7babc6a216bab7016
Ian Huntington <ian.huntington@plumguide.com>
2022-11-18 13:41:26 +0000

    Add basic month view for a calendar

--- pages/calendar/calendar.css.ts
+++ pages/calendar/calendar.css.ts
@@ -1,6 +1,35 @@
-import { style } from "@vanilla-extract/css"
+import { globalStyle, style } from "@vanilla-extract/css"
+
+globalStyle("html", {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100%",
+});
+
+globalStyle("body", {
+    display: "flex",
+    flex: 1,
+});
+
+globalStyle("#__next", {
+    display: "flex",
+    flex: 1,
+});
+
+export const page = style({
+    blockSize: "100%",
+    display: "flex",
+    flexDirection: "column",
+    inlineSize: "100%",
+});
 
 export const container = style({
-    padding: 10,
-    border: "1px solid red",
-})
+    border: "1px solid black",
+    display: "grid",
+    flex: 1,
+    gridTemplateColumns: "repeat(7, 1fr)",
+});
+
+export const example = style({
+    padding: "1rem",
+});
--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -1,7 +1,55 @@
-import { container } from "./calendar.css"
+import {
+  eachDayOfInterval,
+  endOfISOWeek,
+  endOfMonth,
+  formatISO,
+  getWeeksInMonth,
+  startOfISOWeek,
+  startOfMonth,
+} from "date-fns";
+import { container, example, page } from "./calendar.css";
+
+import type { PropsWithChildren, ReactElement } from "react";
+
+function DayCell({ date }: { date: Date }) {
+  return (
+    <div>
+      <p>{formatISO(date, { representation: "date" })}</p>
+    </div>
+  );
+}
+
+function Page({ children }: PropsWithChildren): ReactElement {
+  return <div className={page}>{children}</div>;
+}
+
+function Block({ children }: PropsWithChildren) {
+  return <div className={example}>{children}</div>;
+}
 
 export default function Calendar() {
-    return (
-        <p className={container}>Calendar</p>   
-    )
+  const date = new Date(2022, 3, 10);
+  const weeksInMonth = getWeeksInMonth(date);
+  const startDate = startOfISOWeek(startOfMonth(date));
+  const endDate = endOfISOWeek(endOfMonth(date));
+  const dates = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  });
+
+  return (
+    <Page>
+      <Block>
+        <p>header</p>
+      </Block>
+      <div className={container}>
+        {dates.map((d) => (
+          <DayCell date={d} key={d.toISOString()} />
+        ))}
+      </div>
+      <Block>
+        <p>footer</p>
+      </Block>
+    </Page>
+  );
 }
