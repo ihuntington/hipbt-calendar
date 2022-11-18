40fcefa99649c20c9b985579eb1b9cc60dcd8f91
Ian Huntington <ian.huntington@plumguide.com>
2022-11-18 15:38:37 +0000

    Add week numbers

--- pages/calendar/calendar.css.ts
+++ pages/calendar/calendar.css.ts
@@ -23,8 +23,14 @@ export const page = style({
     inlineSize: "100%",
 });
 
-export const container = style({
-    border: "1px solid black",
+export const calendarView = style({
+    background: "#f1f5f9",
+    display: "grid",
+    flex: 1,
+    gridTemplateColumns: "4rem 1fr",
+});
+
+export const calendar = style({
     display: "grid",
     flex: 1,
     gridTemplateColumns: "repeat(7, 1fr)",
@@ -32,4 +38,14 @@ export const container = style({
 
 export const example = style({
     padding: "1rem",
+});
+
+export const weeksOfYear = style({
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+});
+
+export const weekNumber = style({
+    color: "red",
 });
--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -1,13 +1,22 @@
 import {
   eachDayOfInterval,
+  eachWeekOfInterval,
   endOfISOWeek,
   endOfMonth,
   formatISO,
+  getWeek,
   getWeeksInMonth,
   startOfISOWeek,
   startOfMonth,
 } from "date-fns";
-import { container, example, page } from "./calendar.css";
+import {
+  calendar,
+  calendarView,
+  example,
+  page,
+  weekNumber,
+  weeksOfYear,
+} from "./calendar.css";
 
 import type { PropsWithChildren, ReactElement } from "react";
 
@@ -15,6 +24,34 @@ function DayCell({ date }: { date: Date }) {
   return (
     <div>
       <p>{formatISO(date, { representation: "date" })}</p>
+    </div>
+  );
+}
+
+function WeekOfYear({
+  startDate,
+  endDate,
+}: {
+  startDate: Date;
+  endDate: Date;
+}) {
+  const weeks = eachWeekOfInterval({
+    start: startDate,
+    end: endDate,
+  });
+
+  return (
+    <div className={weeksOfYear}>
+      {weeks.map((w) => {
+        const weekNum = getWeek(w, {
+            weekStartsOn: 1,
+        });
+        return (
+          <p className={weekNumber} key={weekNum}>
+            {weekNum}
+          </p>
+        );
+      })}
     </div>
   );
 }
@@ -28,7 +65,7 @@ function Block({ children }: PropsWithChildren) {
 }
 
 export default function Calendar() {
-  const date = new Date(2022, 3, 10);
+  const date = new Date(2022, 0, 1);
   const weeksInMonth = getWeeksInMonth(date);
   const startDate = startOfISOWeek(startOfMonth(date));
   const endDate = endOfISOWeek(endOfMonth(date));
@@ -42,10 +79,13 @@ export default function Calendar() {
       <Block>
         <p>header</p>
       </Block>
-      <div className={container}>
-        {dates.map((d) => (
-          <DayCell date={d} key={d.toISOString()} />
-        ))}
+      <div className={calendarView}>
+        <WeekOfYear startDate={startDate} endDate={endDate} />
+        <div className={calendar}>
+          {dates.map((d) => (
+            <DayCell date={d} key={d.toISOString()} />
+          ))}
+        </div>
       </div>
       <Block>
         <p>footer</p>
