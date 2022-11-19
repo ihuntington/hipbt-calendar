4b8e24916dc95bf8282b6d7a243148c8f7364f34
Ian Huntington <ian.huntington@plumguide.com>
2022-11-19 17:08:19 +0000

    Update next config to use Preact in production

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const withVanillaExtract = createVanillaExtractPlugin()
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 }
 
 module.exports = withVanillaExtract(nextConfig)
