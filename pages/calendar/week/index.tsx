8c6ed6966f975b716d3bfeccd62c960e3d09f02f
Ian Huntington <ian.huntington@plumguide.com>
2022-12-04 12:32:15 +0000

    Move /week to /calendar/week

--- pages/calendar/week/index.tsx
+++ pages/calendar/week/index.tsx
@@ -1,10 +1,10 @@
-import { App, Flex } from "../../components";
-import { CalendarProvider } from "../../components/Calendar/context/CalendarContext";
-import { WeekHeader, WeekNav, WeekView } from "../../components/Calendar/components";
+import { App, Flex } from "../../../components";
+import { CalendarProvider } from "../../../components/Calendar/context/CalendarContext";
+import { WeekHeader, WeekNav, WeekView } from "../../../components/Calendar/components";
 
 import * as styles from "./index.css";
 
-import "../../styles/global.css";
+import "@/styles/global.css";
 
 function Page({ children }: React.PropsWithChildren): React.ReactElement {
 	return <div className={styles.page}>{children}</div>;
